7a66e4117831cfb9b90b0f4eac5078ac6a5a632b
Lena Plaksina <lena.plaksina@gmail.com>
2016-02-22 11:05:34 +1300

    Count after a single insertion

--- hash_map.rb
+++ hash_map.rb
@@ -2,4 +2,8 @@ class HashMap
   def insert(key, value)
     true
   end
+
+  def count
+    1
+  end
 end
--- spec/hash_map_spec.rb
+++ spec/hash_map_spec.rb
@@ -6,4 +6,13 @@ RSpec.describe HashMap do
       expect(HashMap.new.insert("apples", 1)).to eq true
     end
   end
+
+  describe "#count" do
+    it "returns a count of 1 after inserting 1 key value pair" do
+      hash_map = HashMap.new
+      hash_map.insert("apples", 1)
+
+      expect(hash_map.count).to eq 1
+    end
+  end
 end
